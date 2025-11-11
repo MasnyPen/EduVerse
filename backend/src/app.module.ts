9656a1ac2a890aa138546af2a5feb399c3696f1d
MasnyPen <piotrek9868@gmail.com>
2025-11-11 09:08:15 +0100

    add conections to databases, compose and validation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { redisStore } from 'cache-manager-redis-store';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // ConfigModule
+    ConfigModule.forRoot(),
+    // Connect to MongoDB database
+    MongooseModule.forRoot(process.env.MONGO_URI || "mongodb://localhost:27017/nestdb", {}),
+    // Connect to Redis
+    CacheModule.registerAsync({
+      useFactory: async () => ({
+        store: await redisStore({
+          socket: {
+            host: process.env.REDIS_HOST || 'localhost',
+            port: process.env.REDIS_PORT || 6379,
+          },
+          ttl: 60,
+        }),
+      }),
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
